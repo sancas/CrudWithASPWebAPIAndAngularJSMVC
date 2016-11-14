696d53a2ac5acef63c77bb27ff7adaa7aeb49e93
Cristian Sanchez <sancas19@gmail.com>
2016-11-14 15:34:30 -0600

    Agregados algunos cosas del AngularJS

---
+++ AngularJSMVC_Client/scripts/controllers/controllers.js
@@ -0,0 +1,44 @@
+﻿app.controller('ProductListCtrl', ['$scope', 'ProductsFactory', 'ProductFactory', '$location',
+function ($scope, ProductsFactory, ProductFactory, $location) {
+    $scope.editProduct = function (productId) {
+        $location.path('/product-detail/' + productId);
+    };
+
+    $scope.deleteProduct = function (productId) {
+        var result = confirm('Esta seugro?');
+        if (result == true) {
+            ProductFactory.delete({ id: productId });
+            $scope.products = ProductsFactory.query();
+        }
+    };
+
+    $scope.createNewProduct = function () {
+        $location.path('/product-creation');
+    };
+
+    $scope.products = ProductsFactory.query();
+}]);
+
+app.controller('ProductDetailCtrl', ['$scope', '$routeParams', 'ProductsFactory', 'ProductFactory', '$location',
+    function ($scope, $routeParams, ProductsFactory, ProductFactory, $location) {
+        $scope.updateProduct = function () {
+            ProductFactory.update($scope.product);
+            $scope.products = ProductsFactory.query();
+            $location.path('/product-list');
+        };
+
+        $scope.cancel = function () {
+            $location.path('/product-list');
+        };
+
+        $scope.product = ProductFactory.show({ id: $routeParams.id });
+    }]);
+
+app.controller('ProductCreationCtrl', ['$scope', 'ProductsFactory', '$location',
+    function ($scope, ProductsFactory, $location) {
+        $scope.createNewProduct = function () {
+            ProductsFactory.create($scope.product);
+            $scope.products = ProductsFactory.query();
+            $location.path('/product-list');
+        }
+    }]);
---
+++ AngularJSMVC_Client/scripts/services/productService.js
@@ -0,0 +1,18 @@
+﻿app.factory('ProductsFactory', function ($resource) {
+    this.BASE_URL = 'http://localhost:57029/api/product/';
+
+    return $resource(this.BASE_URL, {}, {
+        query: { method: 'GET', isArray: true },
+        create: { method: 'POST' }
+    })
+});
+
+app.factory('ProductFactory', function ($resource) {
+    this.BASE_URL = 'http://localhost:57029/api/product/';
+
+    return $resource(this.BASE_URL + ':id', {}, {
+        show: { method: 'GET' },
+        update: { method: 'PUT' },
+        delete: { method: 'DELETE', params: { id: '@id' } }
+    })
+});
