b1e50dd0b114b53d40200767602fe63a34fa03e5
Cristian Sanchez <sancas19@gmail.com>
2016-11-14 14:33:23 -0600

    Se creo el modelo y se creo un contrador en blanco

---
+++ CrudWithASPWebAPIAndAngularJSMVC/Controllers/ProductController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using CrudWithASPWebAPIAndAngularJSMVC.Models;
+
+namespace CrudWithASPWebAPIAndAngularJSMVC.Controllers
+{
+    [RoutePrefix("api/product")]
+    public class ProductController : ApiController
+    {
+
+    }
+}
